47eb1bc932323a13226dfa0412b5cbcd8b26a9e0
DJ Price <djpric91@gmail.com>
2024-11-06 04:29:10 +0000

    spruce it up

--- index2.ts
+++ index2.ts
@@ -1,7 +1,12 @@
+/**
+ * Types
+ */
 type Vector2 = { x: number; y: number; }
-
 type Vector3 = { x: number; y: number; z: number; }
 
+/**
+ * Magnitude functions
+ */
 const vector2Magnitude = (vec: Vector2) => {
     return Math.sqrt(
         vec.x * vec.x +
@@ -17,6 +22,9 @@ const vector3Magnitude = (vec: Vector3) => {
     )
 }
 
+/**
+ * Test objects
+ */
 const testVector2: Vector2 = {
     x: 3,
     y: 4
@@ -37,12 +45,20 @@ const someOtherWeirdObject = {
     baz: undefined
 }
 
+/**
+ * Log some results
+ */
 console.log(vector3Magnitude(testVector3))
 console.log(vector3Magnitude(someOtherWeirdObject))
 
 console.log(vector2Magnitude(testVector2))
 console.log(vector2Magnitude(testVector3))
 
+console.log("\n\n--------------------------------\n\n")
+
+/**
+ * Type inference functions
+ */
 const isVector3 = (v: object): v is Vector3 => {
     if(
         "x" in v && typeof v.x === "number" &&
@@ -62,6 +78,9 @@ const isVector2 = (v: object): v is Vector2 => {
     return false;
 }
 
+/**
+ * Higher order function
+ */
 const doSomethingToVector = <V2Output, V3Output>(
     v: object,
     vector2Operation: (input: Vector2) => V2Output,
@@ -76,6 +95,9 @@ const doSomethingToVector = <V2Output, V3Output>(
     return null;
 }
 
+/**
+ * Get outputs
+ */
 let nullOutput, v2Output, v3Output;
 
 nullOutput = doSomethingToVector(
@@ -100,6 +122,29 @@ console.log("v3 output: ", v3Output)
 
 console.log("\n\n--------------------------------\n\n")
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * Fixed higher order function
+ */
 const doSomethingToVector_BETTER = <V2Output, V3Output>(
     v: object,
     vector2Operation: (input: Vector2) => V2Output,
